68bb95d42d389527cb95a016de6ac03fee008ffe
dandavies23 <dan@dandavies23.com>
2021-11-26 12:39:14 +0000

    Add Register functionality

--- app.py
+++ app.py
@@ -45,8 +45,6 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-        return redirect(url_for("profile", username=session["user"]))
-
     return render_template("register.html")
 
 
