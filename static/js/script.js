df024b9acb09d3f478024cc638f6c0a0491e8d52
dandavies23 <dan@dandavies23.com>
2021-12-05 10:30:22 +0000

    Add defensive programming modal alert to Categories

--- static/js/script.js
+++ static/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
   $("select").formSelect();
   $(".tooltipped").tooltip();
   $(".scrollspy").scrollSpy();
+  $('.modal').modal();
   validateMaterializeSelect();
   function validateMaterializeSelect() {
       let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
